8cf2a717300de5f4290e8f101d28ca60d9d8982c
Facundo S. Ramos <66887750+facundoramos-dev@users.noreply.github.com>
2023-12-05 17:51:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMembers() {
+  return [
+    {
+      id: 1,
+      name: 'Samuel',
+      skills: ['Node.js', 'React', 'TypeScript', 'CSS', 'HTML'],
+    },
+    {
+      id: 2,
+      name: 'João',
+      skills: ['Java', 'Jenkins', 'Python', 'Docker'],
+    },
+    {
+      id: 3,
+      name: 'Pedro',
+      skills: ['React', 'React Native', 'Flutter', 'Ionic'],
+    },
+    {
+      id: 4,
+      name: 'Ana',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Node.js'],
+    },
+  ];
+}
